7697e698c0a32b2335f2bf47797f9d59e625fd7a
Kevin Hellemun <17928966+OGKevin@users.noreply.github.com>
2025-09-21 22:11:04 -0400

    feat: add ComicMeta button and fix site URL typo
    Added ComicMeta button to homepage and updated CSS for spacing.
    Fixed typo in site URL in Docusaurus config for correct deployment.
    
    Change-Id: cec7fa3b0873ff4aa87c4f004f0165a0
    Change-Id-Short: nlnskpwozrsw

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -19,7 +19,7 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: "https://korcomic.github.com",
+  url: "https://korcomic.github.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/docs",
